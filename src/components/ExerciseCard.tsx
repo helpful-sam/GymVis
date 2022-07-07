f6b5b6a2affea63f3fe199d86405a83a5fb49583
Pepijn <pvwpvw@protonmail.com>
2022-07-07 15:07:17 +0200

    Fixed X icon positioning in cards

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -5,117 +5,116 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CloseIcon from '@mui/icons-material/Close';
-import { Box, IconButton } from '@mui/material';
+import { Box, IconButton, makeStyles } from '@mui/material';
 
 interface ExerciseClean {
-  label: string;
-  target: string[];
-  synergists: string[];
-  dynamicStabilizers: string[];
-  stabilizers: string[];
-  antagonistStabilizer: string[];
-  prep: string;
-  exec: string;
-  mechanics: string;
-  force: string;
-  url: string;
+    label: string;
+    target: string[];
+    synergists: string[];
+    dynamicStabilizers: string[];
+    stabilizers: string[];
+    antagonistStabilizer: string[];
+    prep: string;
+    exec: string;
+    mechanics: string;
+    force: string;
+    url: string;
 }
 
 interface ExerciseArr {
-  exer: {
-  label: string;
-  target: string[];
-  synergists: string[];
-  dynamicStabilizers: string[];
-  stabilizers: string[];
-  antagonistStabilizer: string[];
-  prep: string;
-  exec: string;
-  mechanics: string;
-  force: string;
-  url: string;},
-  onRemove:(exer: ExerciseClean) => void
+    exer: {
+        label: string;
+        target: string[];
+        synergists: string[];
+        dynamicStabilizers: string[];
+        stabilizers: string[];
+        antagonistStabilizer: string[];
+        prep: string;
+        exec: string;
+        mechanics: string;
+        force: string;
+        url: string;
+    },
+    onRemove: (exer: ExerciseClean) => void
 }
 
 
-export default function ExerciseCard({ exer, onRemove }:ExerciseArr) {
 
-  function arrayToString(muscles: string[]) {
-    let musclesSeperated = ""
-    for (let muscle of muscles) {
-        if (musclesSeperated) {
-            musclesSeperated = musclesSeperated + " & " + muscle
-        } else {
-            musclesSeperated = muscle
+
+export default function ExerciseCard({ exer, onRemove }: ExerciseArr) {
+    function arrayToString(muscles: string[]) {
+        let musclesSeperated = ""
+        for (let muscle of muscles) {
+            if (musclesSeperated) {
+                musclesSeperated = musclesSeperated + " & " + muscle
+            } else {
+                musclesSeperated = muscle
+            }
         }
+
+        return musclesSeperated
     }
 
-    return musclesSeperated
-  }
+    return (
+        <>
+            <Accordion>
+                <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+                    <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header">
+                        <Typography>{exer.label}</Typography>
+                    </AccordionSummary>
+                    <Box sx={{ display: "flex" }}>
+                        <IconButton disableRipple={true} onClick={() => onRemove(exer)}>
+                            <CloseIcon fontSize="small" />
+                        </IconButton>
+                    </Box>
+                </Box>
+                <AccordionDetails>
+                    {exer.target[0] ?
+                        exer.target[1] ?
+                            <div className="textContainer"><h5 className="title">Target muscles: </h5><h5 className="tekst">{arrayToString(exer.target)}</h5></div>
+                            :
+                            <div className="textContainer"><h5 className="title">Target muscle: </h5><h5 className="tekst">{arrayToString(exer.target)}</h5></div>
+                        :
+                        ""
+                    }
 
-  return (
-    <>
-      <Accordion>
-        <Box sx={{ display: "flex", width: "90%" }}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography>{exer.label}</Typography>
-          </AccordionSummary>
-          <Box>
-            <IconButton onClick={() => onRemove(exer)}>
-              <CloseIcon/>
-            </IconButton>
-          </Box>
-        </Box>
-        <AccordionDetails>
-            {exer.target[0] ?
-            exer.target[1] ?
-            <div className="textContainer"><h5 className="title">Target muscles: </h5><h5 className="tekst">{arrayToString(exer.target)}</h5></div>
-            :
-            <div className="textContainer"><h5 className="title">Target muscle: </h5><h5 className="tekst">{arrayToString(exer.target)}</h5></div>
-            :
-            ""
-            }
+                    {exer.synergists[0] ?
+                        exer.synergists[1] ?
+                            <div className="textContainer"><h5 className="title">Synergist muscles:</h5><h5 className="tekst">{arrayToString(exer.synergists)}</h5></div>
+                            :
+                            <div className="textContainer"><h5 className="title">Synergist muscle:</h5><h5 className="tekst">{arrayToString(exer.synergists)}</h5></div>
+                        :
+                        ""}
 
+                    {exer.stabilizers[0] ?
+                        exer.stabilizers[1] ?
+                            <div className="textContainer"><h5 className="title">Stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.stabilizers)}</h5></div>
+                            :
+                            <div className="textContainer"><h5 className="title">Stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.stabilizers)}</h5></div>
+                        :
+                        ""}
 
-            {exer.synergists[0] ?
-            exer.synergists[1] ?
-            <div className="textContainer"><h5 className="title">Synergist muscles:</h5><h5 className="tekst">{arrayToString(exer.synergists)}</h5></div>
-            :
-            <div className="textContainer"><h5 className="title">Synergist muscle:</h5><h5 className="tekst">{arrayToString(exer.synergists)}</h5></div>
-            :
-            ""}
+                    {exer.dynamicStabilizers[0] ?
+                        exer.dynamicStabilizers[1] ?
+                            <div className="textContainer"><h5 className="title">Dynamic stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.dynamicStabilizers)}</h5></div>
+                            :
+                            <div className="textContainer"><h5 className="title">Dynamic stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.dynamicStabilizers)}</h5></div>
+                        :
+                        ""}
 
-            {exer.stabilizers[0] ?
-            exer.stabilizers[1] ?
-            <div className="textContainer"><h5 className="title">Stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.stabilizers)}</h5></div>
-            :
-            <div className="textContainer"><h5 className="title">Stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.stabilizers)}</h5></div>
-            :
-            ""}
-
-            {exer.dynamicStabilizers[0] ?
-            exer.dynamicStabilizers[1] ?
-            <div className="textContainer"><h5 className="title">Dynamic stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.dynamicStabilizers)}</h5></div>
-            :
-            <div className="textContainer"><h5 className="title">Dynamic stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.dynamicStabilizers)}</h5></div>
-            :
-            ""}
-
-            {exer.antagonistStabilizer[0] ?
-            exer.antagonistStabilizer[1] ?
-            <div className="textContainer"><h5 className="title">Antagonistic stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.antagonistStabilizer)}</h5></div>
-            :
-            <div className="textContainer"><h5 className="title">Antagonistic stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.antagonistStabilizer)}</h5></div>
-            :
-            ""}
-            <div className="textContainer"><h5 className="title">Force: </h5><h5 className="tekst">{exer.force}</h5></div>
-
-        </AccordionDetails>
-      </Accordion>
-    </>
-  );
+                    {exer.antagonistStabilizer[0] ?
+                        exer.antagonistStabilizer[1] ?
+                            <div className="textContainer"><h5 className="title">Antagonistic stabilizing muscles:</h5><h5 className="tekst">{arrayToString(exer.antagonistStabilizer)}</h5></div>
+                            :
+                            <div className="textContainer"><h5 className="title">Antagonistic stabilizing muscle:</h5><h5 className="tekst">{arrayToString(exer.antagonistStabilizer)}</h5></div>
+                        :
+                        ""}
+                    <div className="textContainer"><h5 className="title">Force: </h5><h5 className="tekst">{exer.force}</h5></div>
+                </AccordionDetails>
+            </Accordion>
+        </>
+    );
 }
