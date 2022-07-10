a15d048c3c479c9e1dd05937b3571d96911218b8
Pepijn <pvwpvw@protonmail.com>
2022-07-10 15:35:17 +0200

    Added mechanics and force in exercise info

--- src/components/ActiveExercise.tsx
+++ src/components/ActiveExercise.tsx
@@ -33,19 +33,21 @@ export default function Active({ exer }: Exercise) {
         <div className="active">
             {exer.label ? <h1>{exer.label}</h1> : ""}
 
-            {exer.prep ? <><h2>Preperation:</h2> <h3>{exer.prep}</h3></> : ""}
+            {exer.mechanics && exer.force ? <h3 className="activeText">{exer.mechanics} {exer.force}</h3> : ""}
 
-            {exer.exec ? <><h2>Execution:</h2><h3>{exer.exec}</h3></> : ""}
+            {exer.prep ? <><h2>Preperation:</h2> <h3 className="activeText">{exer.prep}</h3></> : ""}
 
-            {exer.target[0] ? <><h2>Target:</h2><h3>{arrayToString(exer.target)}</h3></> : ""}
+            {exer.exec ? <><h2>Execution:</h2><h3 className="activeText">{exer.exec}</h3></> : ""}
 
-            {exer.synergists[0] ? <><h2>Synergist:</h2><h3>{arrayToString(exer.synergists)}</h3></> : ""}
+            {exer.target[0] ? <><h2>Target:</h2><h3 className="activeText">{arrayToString(exer.target)}</h3></> : ""}
 
-            {exer.stabilizers[0] ? <><h2>Stabilizer:</h2><h3>{arrayToString(exer.stabilizers)}</h3></> : ""}
+            {exer.synergists[0] ? <><h2>Synergist:</h2><h3 className="activeText">{arrayToString(exer.synergists)}</h3></> : ""}
 
-            {exer.dynamicStabilizers[0] ? <><h2>Dynamic stabilizer:</h2><h3>{arrayToString(exer.dynamicStabilizers)}</h3></> : ""}
+            {exer.stabilizers[0] ? <><h2>Stabilizer:</h2><h3 className="activeText">{arrayToString(exer.stabilizers)}</h3></> : ""}
 
-            {exer.antagonistStabilizer[0] ? <><h2>Antagonistic stabilizer:</h2><h3>{arrayToString(exer.antagonistStabilizer)}</h3></> : ""}
+            {exer.dynamicStabilizers[0] ? <><h2>Dynamic stabilizer:</h2><h3 className="activeText">{arrayToString(exer.dynamicStabilizers)}</h3></> : ""}
+
+            {exer.antagonistStabilizer[0] ? <><h2>Antagonistic stabilizer:</h2><h3 className="activeText">{arrayToString(exer.antagonistStabilizer)}</h3></> : ""}
         </div>
     );
 }
