5c04e2cb6d570c01edcef8ef7219aefde04deac3
Pepijn <pvwpvw@protonmail.com>
2022-07-16 23:43:51 +0200

    Created interface props

---
+++ src/components/InterfaceExercise.tsx
@@ -0,0 +1,17 @@
+interface Exercise {
+    exer: {
+        label: string;
+        target: string[];
+        synergists: string[];
+        dynamicStabilizers: string[];
+        stabilizers: string[];
+        antagonistStabilizer: string[];
+        prep: string;
+        exec: string;
+        mechanics: string;
+        force: string;
+        url: string;
+    },
+}
+
+export default Exercise
---
+++ src/components/InterfaceExerciseClean.tsx
@@ -0,0 +1,17 @@
+// Exercise interface.
+interface ExerciseClean {
+    visib: boolean;
+    label: string;
+    target: string[];
+    synergists: string[];
+    dynamicStabilizers: string[];
+    stabilizers: string[];
+    antagonistStabilizer: string[];
+    prep: string;
+    exec: string;
+    mechanics: string;
+    force: string;
+    url: string;
+}
+
+export default ExerciseClean
